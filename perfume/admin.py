bf1cddae4b152a6027bff13671fdfa6020782b3d
Van Tu <vtu7410@gmail.com>
2024-03-07 10:40:49 -0800

    first commit to github

---
+++ perfume/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Designer, Note, Accord, Perfume
+
+
+# Register your models here.
+admin.site.register(Designer)
+admin.site.register(Note)
+admin.site.register(Accord)
+admin.site.register(Perfume)
---
+++ perfume/models.py
@@ -0,0 +1,54 @@
+# Create your models here.
+from django.db import models
+from django.conf import settings
+from taggit.managers import TaggableManager
+
+
+# Create your models here.
+# Models are saved in the database
+# Think of a model in the database as a spreadsheet with columns (fields) and rows (data)
+class Designer(models.Model):
+    name = models.CharField(max_length=200)
+    logo = models.URLField(max_length=300)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Note(models.Model):
+    note = models.CharField(max_length=200)
+    image = models.URLField(max_length=300)
+
+    def __str__(self) -> str:
+        return self.note
+
+
+class Accord(models.Model):
+    accord = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.accord
+
+
+class Perfume(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.URLField(max_length=300)
+
+    # a designer can have many perfumes, but perfume can only have 1 designer
+    designer = models.ForeignKey(Designer, on_delete=models.RESTRICT)
+
+    summary = models.TextField(
+        max_length=1000, help_text="Enter a brief description of the perfume"
+    )
+    tags = TaggableManager()
+
+    notes = models.ManyToManyField(Note)
+    accords = models.ManyToManyField(Accord)
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return f"{self.name} by {self.designer}"
