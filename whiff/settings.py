06292d25fe66362bf9578b594223d09fdf3bc06c
Van Tu <vtu7410@gmail.com>
2024-03-08 16:36:20 -0800

    fourth commit

--- whiff/settings.py
+++ whiff/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = [
     "127.0.0.1",
     ".pythonanywhere.com",
     "localhost",
-    "https://whiff-backend-5f278bf19e19.herokuapp.com/",
+    ".whiff-backend-5f278bf19e19.herokuapp.com",
 ]
 
 
