314271abbbfdca8c7c5fec58edc84944bd3c678d
Van Tu <vtu7410@gmail.com>
2024-03-19 13:00:38 -0700

    changed meta for designer

--- perfume/models.py
+++ perfume/models.py
@@ -16,7 +16,7 @@ class Designer(models.Model):
         ordering = ["name"]
 
     def __str__(self) -> str:
-        return self.name + ", pk= " + str(self.id)
+        return self.name
 
 
 class Note(models.Model):
